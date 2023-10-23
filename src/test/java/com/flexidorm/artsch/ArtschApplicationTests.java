2efc047c0e964a62087a22dcaa8dfb7a7c801395
Sebastian Montes <u201712366@upc.edu.pe>
2023-10-23 17:57:47 -0500

    feat: implementacion inicial

---
+++ src/test/java/com/flexidorm/artsch/ArtschApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flexidorm.artsch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArtschApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
